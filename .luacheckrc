ac5b617d258ba04c4b60fe37c0cf7f3ac4cb52f1
Mat Jones <mat@mjones.network>
2026-09-10 12:10:16 -0400

    chore: initial revision of template repo

---
+++ .luacheckrc
@@ -0,0 +1,5 @@
+std = "lua51"
+globals = { "vim" }
+max_line_length = false
+codes = true
+exclude_files = { ".tmp/*", "deps/*" }
---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        packages = with pkgs; [
+          stylua
+          selene
+          just
+          neovim
+          lua-language-server
+          lua51Packages.nlua
+          lua51Packages.busted
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "backend-template";
+          inherit packages;
+        };
+        devShells.ci = pkgs.mkShell {
+          name = "ci";
+          inherit packages;
+        };
+      }
+    );
+}
